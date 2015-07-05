a6a45ccf22f6ba69abd7448e8304a6506cd8935c
Roy de Jong <roy@softwarepunt.nl>
2015-07-05 21:34:01 +0200

    PHPUnit configuration

---
+++ phpunit.php
@@ -0,0 +1,8 @@
+<?php
+
+/**
+ * Bootstrapper for PHPUnit environment.
+ */
+
+// Initialize the class autoloader
+require_once __DIR__ . '/vendor/autoload.php';
---
+++ tests/DummyTest.php
@@ -0,0 +1,9 @@
+<?php
+
+class DummyTest extends PHPUnit_Framework_TestCase
+{
+    public function testUnitTestDummyTest()
+    {
+        $this->assertTrue(true, 'Logic');
+    }
+}
