8198a430137a8338d802d7c958e249f3942f549f
Roy de Jong <roy@softwarepunt.nl>
2015-07-06 21:14:58 +0200

    Routing: Add unit test for Router

---
+++ tests/Routing/RouterTest.php
@@ -0,0 +1,48 @@
+<?php
+
+use Enlighten\Http\Request;
+use Enlighten\Routing\Route;
+use Enlighten\Routing\Router;
+
+class RouterTest extends PHPUnit_Framework_TestCase
+{
+    public function testBlankRouter()
+    {
+        $request = new Request();
+        $request->setRequestUri('/');
+
+        $router = new Router();
+        $this->assertNull($router->route($request));
+    }
+
+    public function testSimpleRouteMatch()
+    {
+        $route = new Route('/', function () {
+            // ...
+        });
+
+        $request = new Request();
+        $request->setRequestUri('/');
+
+        $router = new Router();
+        $router->register($route);
+
+        $this->assertEquals($route, $router->route($request));
+    }
+
+    public function testRouterClear()
+    {
+        $route = new Route('/', function () {
+            // ...
+        });
+
+        $request = new Request();
+        $request->setRequestUri('/');
+
+        $router = new Router();
+        $router->register($route);
+        $router->clear();
+
+        $this->assertNull($router->route($request));
+    }
+}
