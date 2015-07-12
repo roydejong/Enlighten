2448e255a7c4b3030953c66fe3c99db759e1eea0
Roy de Jong <roy@softwarepunt.nl>
2015-07-12 21:27:56 +0200

    [#3, #10] Response: Add support for configuring + sending Cookies. Refactor code + phpdocs.

--- lib/Http/Response.php
+++ lib/Http/Response.php
@@ -8,10 +8,16 @@ namespace Enlighten\Http;
  */
 class Response
 {
+    /**
+     * The protocol that will be specified when sending response headers.
+     * This is a constant value based on the HTTP spec that has been implemented in the framework code.
+     *
+     * @var string
+     */
     const PROTOCOL_VERSION = 'HTTP/1.1';
 
     /**
-     * The HTTP status code
+     * The HTTP status code to be sent to the client.
      *
      * @var int
      */
@@ -25,7 +31,15 @@ class Response
     protected $headers;
 
     /**
-     * Raw body string.
+     * The cookies to be sent to the client.
+     * Key/value array containing name => Cookie.
+     *
+     * @var Cookie[]
+     */
+    protected $cookies;
+
+    /**
+     * Raw body string to be sent to the client.
      *
      * @var string
      */
@@ -38,11 +52,15 @@ class Response
     {
         $this->statusCode = ResponseCode::HTTP_OK;
         $this->headers = [];
+        $this->cookies = [];
         $this->body = '';
     }
 
     /**
+     * Sets the HTTP response code.
+     *
      * @param int $code
+     * @return $this
      */
     public function setResponseCode($code)
     {
@@ -51,10 +69,11 @@ class Response
         }
 
         $this->statusCode = $code;
+        return $this;
     }
 
     /**
-     * Returns the HTTP response status code.
+     * Gets the HTTP response status code.
      *
      * @return int
      */
@@ -69,11 +88,13 @@ class Response
      *
      * @param string $url
      * @param bool $permanent If true, a 301 Moved Permanently will be issued; otherwise a 307 Temporary Redirect.
+     * @return $this
      */
     public function doRedirect($url, $permanent = false)
     {
         $this->setResponseCode($permanent ? ResponseCode::HTTP_MOVED_PERMANENTLY : ResponseCode::HTTP_TEMPORARY_REDIRECT);
         $this->setHeader('Location', $url);
+        return $this;
     }
 
     /**
@@ -81,14 +102,16 @@ class Response
      *
      * @param string $key
      * @param string $value
+     * @return $this
      */
     public function setHeader($key, $value)
     {
         $this->headers[strval($key)] = strval($value);
+        return $this;
     }
 
     /**
-     * Returns the current value of a given header.
+     * Gets the current value of a given header.
      *
      * @param string $key
      * @return string|null The string value of the header, or null if the header was not set.
@@ -106,30 +129,82 @@ class Response
      * Sets the response body to a certain value.
      *
      * @param string $body
+     * @return $this
      */
     public function setBody($body)
     {
         $this->body = strval($body);
+        return $this;
     }
 
     /**
      * Appends to the response body.
      *
      * @param string $body
+     * @return $this
      */
     public function appendBody($body)
     {
         $this->body .= strval($body);
+        return $this;
     }
 
     /**
-     * Returns the current value of the response body.
+     * Gets the current value of the response body.
      *
      * @return string
      */
     public function getBody()
     {
         return $this->body;
+    }
+
+    /**
+     * Creates or updates a cookie.
+     *
+     * @param string $name Cookie name.
+     * @param string $value Cookie value.
+     * @param int $expire Unix expiration timestamp, or 0 for session expire.
+     * @param string|null $path The path the cookie can be used on.
+     * @param string|null $domain The domain the cookie can be used on.
+     * @param bool $secure True for HTTPS only.
+     * @param bool $httpOnly True for HTTP(S) only (not accessible to JavaScript, etc).
+     * @return $this
+     */
+    public function setCookie($name, $value, $expire = 0, $path = null, $domain = null, $secure = false, $httpOnly = false)
+    {
+        return $this->addCookie((new Cookie())
+            ->setName($name)
+            ->setValue($value)
+            ->setExpireTimestamp($expire)
+            ->setPath($path)
+            ->setDomain($domain)
+            ->setSecure($secure)
+            ->setHttpOnly($httpOnly));
+    }
+
+    /**
+     * Adds a cookie to be sent.
+     *
+     * @param Cookie $cookie
+     * @return $this
+     */
+    public function addCookie(Cookie $cookie)
+    {
+        $this->cookies[$cookie->getName()] = $cookie;
+        return $this;
+    }
+
+    /**
+     * Sends the HTTP cookies to the client.
+     * Causes output!
+     */
+    protected function sendCookies()
+    {
+        foreach ($this->cookies as $cookie) {
+            setcookie($cookie->getName(), $cookie->getValue(), $cookie->getExpireTimestamp(), $cookie->getPath(),
+                $cookie->getDomain(), $cookie->isSecure(), $cookie->isHttpOnly());
+        }
     }
 
     /**
@@ -161,6 +236,7 @@ class Response
     public function send()
     {
         $this->sendHeaders();
+        $this->sendCookies();
 
         if (ResponseCode::canHaveBody($this->statusCode)) {
             $this->sendBody();
