f91d4117a52bfa834c7df209e9ff30e1da4cdb2e
Roy de Jong <roy@softwarepunt.nl>
2015-07-20 21:44:35 +0200

    [#17] RoutingContextTest: HHVM supports determining default values via reflection for closures, adjust tests accordingly

--- tests/Routing/RoutingContextTest.php
+++ tests/Routing/RoutingContextTest.php
@@ -11,7 +11,26 @@ function sampleFunction($nullMeBro, $bogusParam = 'abc', Request $request = null
 
 class RoutingContextTest extends PHPUnit_Framework_TestCase
 {
-    public function testRegisterAndPassObjectInstanceWithClosure()
+    /**
+     * Determines whether this version of PHP is capable of determining default parameter values for closures via
+     * reflection.
+     *
+     * @return bool
+     */
+    public function shouldSupportClosureDefaultValues()
+    {
+        if (defined('HHVM_VERSION')) {
+            // Running HHVM; this should work
+            return true;
+        }
+
+        // Confirmed not to work in PHP 5.5
+        // Confirmed not to work in PHP 5.6
+        // Confirmed not to work in PHP 7.0.0-dev nightly (tested 18 jul)
+        return false;
+    }
+
+    public function testInjectionWithClosureFunc()
     {
         $request = new Request();
         $request->setRequestUri('/hello');
@@ -26,20 +45,20 @@ class RoutingContextTest extends PHPUnit_Framework_TestCase
         $paramList = $context->determineValues($myFunction);
 
         $expectedParams = [
-            null,
+            $this->shouldSupportClosureDefaultValues() ? '123' : null,
             $request,
             null
         ];
 
         // NB: Unfortunately, default values (e.g. "123" in the example above) in combination with closures are not
-        // supported because of the way ReflectionParameter is implemented in PHP . Should that ever change, this test
-        // will break.
+        // supported because of the way ReflectionParameter is implemented in PHP. Should that ever change, this test
+        // will break (see "shouldSupportClosureDefaultValues"). HHVM does implement this.
 
         $this->assertEquals($expectedParams, $paramList);
         $this->assertEquals('/hello', call_user_func_array($myFunction, $paramList));
     }
 
-    public function testFunctionByStringWithDefaultValues()
+    public function testInjectionWithStringFunc()
     {
         $request = new Request();
         $request->setRequestUri('/hello');
@@ -61,7 +80,7 @@ class RoutingContextTest extends PHPUnit_Framework_TestCase
         $this->assertEquals('/hello', call_user_func_array($myFunction, $paramList));
     }
 
-    public function testFunctionByArrayWithDefaultValues()
+    public function testInjectionWithArrayFunc()
     {
         $request = new Request();
         $request->setRequestUri('/hello');
